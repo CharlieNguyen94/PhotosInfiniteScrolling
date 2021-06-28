af62567a7a1aad8eb0f38e7440ce28d1207e9ec2
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-06-28 13:19:27 +0100

    First unit test for PhotosVC completed.

--- PhotosInfiniteScrollingTests/PhotosViewModelTests.swift
+++ PhotosInfiniteScrollingTests/PhotosViewModelTests.swift
@@ -10,7 +10,7 @@ import RxSwift
 @testable import PhotosInfiniteScrolling
 
 class PhotosViewModelTests: XCTestCase {
-
+    
     // MARK: - System Under Test
     private var disposeBag: DisposeBag!
     private var sut: PhotosViewModelImplementation!
@@ -35,5 +35,49 @@ class PhotosViewModelTests: XCTestCase {
         sut = nil
         navigationController = nil
         super.tearDown()
+    }
+    
+    // MARK: - View Model Tests
+    func testWhenWillDisplayCellTriesToLoadData() {
+        
+        let expectation = XCTestExpectation(description: "Load image data")
+        
+        // Initial Mock Data
+        sut.unsplashPhotos.accept(
+            [
+                UnsplashPhoto.init(id: "",
+                                   description: "",
+                                   altDescription: "",
+                                   urls: Urls(
+                                    raw: "https://unsplash.com/photos/wxZJ-V6DPKc",
+                                    full: "https://unsplash.com/photos/wxZJ-V6DPKc",
+                                    regular: "https://unsplash.com/photos/wxZJ-V6DPKc",
+                                    small: "https://unsplash.com/photos/wxZJ-V6DPKc",
+                                    thumb: "https://unsplash.com/photos/wxZJ-V6DPKc")
+                )
+            ]
+        )
+        
+        sut.willDisplayCellAtIndex
+            .accept(0)
+        
+        sut.imageRetrievedError
+            .observeOn(MainScheduler.asyncInstance)
+            .subscribe(onNext: { (index) in
+                XCTAssertEqual(index, 0)
+                expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+        
+        sut.imageRetrievedSuccess
+            .observeOn(MainScheduler.asyncInstance)
+            .subscribe(onNext: { (image, index) in
+                XCTAssertNotNil(image)
+                XCTAssertEqual(index, 0)
+                expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5.0)
     }
 }
